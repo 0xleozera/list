89ab766861ff53747282a4c06d5167aa959c4b83
leonardorpr <leo.r.pinheiro@ulbra.edu.br>
2019-01-17 01:35:18 -0200

    fix (alert screen) class name & onChange to field

--- src/screens/Alerts/index.js
+++ src/screens/Alerts/index.js
@@ -6,22 +6,24 @@ import TextField from "../../components/TextField";
 import List from "../../components/List";
 import styles from "./styles";
 
-class ActorsScreen extends Component {
+class AlertsScreen extends Component {
   state = {
     data: ActorsData,
     value: ""
   };
+
+  onChange = value => this.setState({ value });
 
   render() {
     const { data, value } = this.state;
 
     return (
       <View style={styles.wrapper}>
-        <TextField value={value} />
+        <TextField value={value} onChange={value => this.onChange(value)} />
         <List data={data} />
       </View>
     );
   }
 }
 
-export default ActorsScreen;
+export default AlertsScreen;
